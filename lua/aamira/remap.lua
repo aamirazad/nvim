004410c0e62908a515a9ee122b44dc232847a4c8
Aamir Azad <aamirmazad@gmail.com>
2022-11-06 22:02:04 -0500

    Still working on stuff

--- lua/aamira/remap.lua
+++ lua/aamira/remap.lua
@@ -1,3 +1,6 @@
 local nnoremap = require("aamira.keymap").nnoremap
 
-nnoremap("<leader>d", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>tr", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>Tr", "<cmd>NvimTreeToggle /home/aamir/.config/nvim<CR>")
+--nnoremap("<leader>fr", "<cmd%s//g<Left><Left>
+
