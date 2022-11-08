aa95722092e63dd8e850bc4495e109acb6f17800
Aamir Azad <aamirmazad@gmail.com>
2022-11-07 21:00:05 -0500

    Rename, add keybind
    Renamed keymap to keyinit, changed replace keybind to cw (change words)

--- lua/aamira/remap.lua
+++ lua/aamira/remap.lua
@@ -1,8 +1,8 @@
-local nnoremap = require("aamira.keymap").nnoremap
+local nnoremap = require("aamira.keyinit").nnoremap
 
 nnoremap("<leader>tr", "<cmd>NvimTreeToggle<CR>")
 nnoremap("<leader>Tr", "<cmd>NvimTreeToggle /home/aamir/.config/nvim<CR>")
-nnoremap("<leader>fr", ":%s//g<Left><Left>")
+nnoremap("<leader>cw", ":%s//g<Left><Left>")
 nnoremap("<leader>js", "<cmd>w<CR><cmd>!clear<CR>:! node %<CR>")
 nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>fi", "<cmd>Telescope live_grep<CR>")  
