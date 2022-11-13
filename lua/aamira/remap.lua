1fa2cc3867b186b34a42271dd57d2c1e529a7ee1
Aamir Azad <aamirmazad@gmail.com>
2022-11-12 22:55:24 -0500

    Update remap.lua

--- lua/aamira/remap.lua
+++ lua/aamira/remap.lua
@@ -3,7 +3,7 @@ local nnoremap = require("aamira.keyinit").nnoremap
 nnoremap("<leader>tr", "<cmd>NvimTreeToggle<CR>")
 nnoremap("<leader>Tr", "<cmd>NvimTreeToggle /home/aamir/.config/nvim<CR>")
 nnoremap("<leader>cw", ":%s//g<Left><Left>")
-nnoremap("<leader>jp", "<cmd>w<CR><cmd>!node %<CR>")
+nnoremap("<leader>js", "<cmd>w<CR><cmd>!node %<CR>")
 nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>fi", "<cmd>Telescope live_grep<CR>")  
 nnoremap("<leader>mf", '<cmd>lua require("harpoon.mark").add_file()<CR>')
