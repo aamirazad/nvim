51c1bd3f9101b936d1a539c772ed749aa459d4bc
Aamir Azad <aamirmazad@gmail.com>
2022-12-22 12:40:37 -0500

    Format packer file

--- lua/aamira/packer.lua
+++ lua/aamira/packer.lua
@@ -1,76 +1,76 @@
 vim.cmd [[packadd packer.nvim]]
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
- return require('packer').startup(function()
-  -- Packer can manage itself
-  use { "wbthomason/packer.nvim" }
-  use 'lewis6991/impatient.nvim'
-  use 'folke/tokyonight.nvim'
-  use 'wakatime/vim-wakatime'
-  use 'nvim-lua/plenary.nvim'
-  use {
-  "max397574/better-escape.nvim",
-  config = function()
-    require("better_escape").setup()
-  end,
-}
-  use {
-  'nvim-tree/nvim-tree.lua',
-  requires = {
-    'nvim-tree/nvim-web-devicons', -- optional, for file icons
-  },
-  tag = 'nightly' -- optional, updated every week. (see issue #1193)
-}
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
-  use 'ThePrimeagen/vim-be-good'
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-}
-use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-use 'nvim-telescope/telescope.nvim'
-use 'lukas-reineke/indent-blankline.nvim'
-use 'ThePrimeagen/harpoon'
-use 'github/copilot.vim'
-use 'mbbill/undotree'
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
-
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
+return require('packer').startup(function()
+    -- Packer can manage itself
+    use { "wbthomason/packer.nvim" }
+    use { 'lewis6991/impatient.nvim' }
+    use { 'folke/tokyonight.nvim' }
+    use { 'wakatime/vim-wakatime' }
+    use { 'nvim-lua/plenary.nvim' }
+    use {
+        "max397574/better-escape.nvim",
+        config = function()
+            require("better_escape").setup()
+        end,
     }
-}
-use {
-  "folke/zen-mode.nvim",
-  config = function()
-    require("zen-mode").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
-  end
-}
-  use('tpope/vim-fugitive')
-  use 'ms-jpq/coq.thirdparty'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use { 'ThePrimeagen/vim-be-good' }
+    use {
+        'goolord/alpha-nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.startify'.config)
+        end
+    }
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use { 'nvim-telescope/telescope.nvim' }
+    use { 'lukas-reineke/indent-blankline.nvim' }
+    use { 'ThePrimeagen/harpoon' }
+    use { 'github/copilot.vim' }
+    use { 'mbbill/undotree' }
+    use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+    use {
+        "folke/zen-mode.nvim",
+        config = function()
+            require("zen-mode").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+    use('tpope/vim-fugitive')
+    use 'ms-jpq/coq.thirdparty'
 end)
 
