88c8632095e1be3f80717d117000fa952f0e1d0c
Aamir Azad <aamirmazad@gmail.com>
2022-12-22 12:44:02 -0500

    Delete config, move everything into their own files!

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,37 @@
+
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.ensure_installed({
+  'eslint',
+  'sumneko_lua',
+  'clangd',
+  'yamlls',
+})
+
+lsp.setup()
+
+require("coq_3p") {
+  { src = "copilot", short_name = "COP", accept_key = "<c-f>" },
+}
+
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+      Lua = {
+          diagnostics = {
+              globals = { 'vim', 'use' }
+          }
+      }
+  }
+}
+
