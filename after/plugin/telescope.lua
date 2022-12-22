4eb8209785497e4f1b23f6cb8133c20eca7a8463
Aamir Azad <aamirmazad@gmail.com>
2022-12-22 12:48:14 -0500

    Clean telescope setup

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,25 +1,3 @@
--- You dont need to set any of these options. These are the default ones. Only
--- the loading is important
-require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
-}
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
-
-
-vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
--- Indent Blankline
-require("indent_blankline").setup {
-    show_end_of_line = true,
-}
-
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fi', builtin.live_grep, {})
