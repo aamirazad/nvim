c4e275517130d8bf29d42e0546a5f6a6bd2b2cc9
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2025-10-16 10:12:50 -0400

    add code error hotkey

--- lua/aamira/plugins.lua
+++ lua/aamira/plugins.lua
@@ -290,6 +290,9 @@ return require("lazy").setup({
 					-- or a suggestion from your LSP for this to activate.
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
+					-- Get code error
+					vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics in floating window" })
+
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
