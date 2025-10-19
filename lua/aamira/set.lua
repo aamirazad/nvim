6c24f3a6387b1ee47c7afddd40fd6d0193d487d1
Aamir Azad <aamirmazad@gmail.com>
2025-10-19 10:15:08 -0400

    tabs not spaces

--- lua/aamira/set.lua
+++ lua/aamira/set.lua
@@ -6,7 +6,8 @@ vim.opt.undofile = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- Don't use spaces
+-- vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.backup = false
